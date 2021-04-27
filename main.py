bcb043cec82534632d3df2734ea9d0f1e6bfc38d
Carlo Abimanyu <66935033+carloabimanyu@users.noreply.github.com>
2021-04-27 18:11:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Proses pengambilan data dari API Jikan
 data = []
-pages = 5
+pages = 2
 for i in range(pages):
     url = 'https://api.jikan.moe/v3/top/anime/{}/'.format(str(i+1))
     response = urlopen(url)
@@ -15,6 +16,7 @@ df = pd.json_normalize(data)
 df['release_year'] = df['start_date'].apply(lambda x: x[-4:]).astype(int)
 
 
+# Halaman awal API dan penjelasan endpoints
 @app.get("/")
 def read_root():
     return {"title":"MyAnimeList Unofficial API (Jikan)",
@@ -32,14 +34,17 @@ def read_root():
                 "/data":"return raw data"
             },
             "type options":"TV, Movie, Special, OVA, ONA, Music (case sensitive)",
+            "season options":"winter, spring, summer, fall (case sensitive)"
         }
 
 
+# Endpoint untuk menampilkan raw data
 @app.get("/data")
 def raw_data():
     return df.to_json(orient='records')
 
 
+# Endpoint untuk menampilkan top 10 anime
 @app.get("/topAnime")
 def read_topAnime():
     col = ['rank', 'title', 'type', 'members', 'score']
@@ -47,6 +52,7 @@ def read_topAnime():
     return result.to_dict(orient='records')
 
 
+# Endpoint untuk menampilkan top 10 anime dengan member terbanyak
 @app.get("/topMemberCount")
 def read_topMemberCount():
     col = ['title', 'type', 'members', 'score']
@@ -54,18 +60,21 @@ def read_topMemberCount():
     return result.to_dict(orient='records')
 
 
+# Endpoint untuk menampilkan rata-rata score berdasarkan tipe
 @app.get("/typeScore")
 def read_typeScore():
     result = df.groupby('type')[['score']].mean()
     return result.to_dict(orient='dict')
 
 
+# Endpoint untuk menampilkan rata-rata score berdasarkan tahun
 @app.get("/yearScore")
 def read_yearScore():
     result = df.groupby('release_year')[['score']].mean()
     return result.to_dict(orient='dict')
 
 
+# Endpoint untuk menampilkan jumlah anime berdasarkan tipe
 @app.get("/typeCount")
 def read_typeCount():
     result = df.groupby('release_year')[['mal_id']].count()
@@ -73,12 +82,14 @@ def read_typeCount():
     return result.to_dict(orient='dict')
 
 
+# Endpoint untuk menampilkan jumlah member berdasarkan tipe
 @app.get("/typeMember")
 def read_typeMember():
     result = df.groupby('type')[['members']].sum()
     return result.to_dict(orient='dict')
 
 
+# Endpoint untuk menampilkan 3 anime terbaik pada season dan tahun tertentu
 @app.get("/bestSeasonalAnime/{year}/{season}")
 def read_bestSeasonalAnime(year: int, season: str):
     seasonal = []
@@ -91,6 +102,7 @@ def read_bestSeasonalAnime(year: int, season: str):
     return result.to_dict(orient='records')
 
 
+# Endpoint untuk mendapatkan rekomendasi anime berdasarkan tahun, tipe, dan score minimal
 @app.get("/recommendation/{tipe}/{year}/{min_score}")
 async def read_recommendation(tipe: str, year: int, min_score: float):
     col = ['rank', 'title', 'members', 'score', 'release_year', 'url']
